95d40c7da8b05697e8377a859dbee6ff779371ca
NaruDevnote <pokeraven4@gmail.com>
2021-08-19 22:30:26 +0100

    refactor(*): move global managers to separate folder

---
+++ src/managers/index.js
@@ -0,0 +1,9 @@
+const AllocationManager = require('./AllocationManager');
+const DatabaseManager = require('./DatabaseManager');
+const FileManager = require('./FileManager');
+
+module.exports = {
+    AllocationManager,
+    DatabaseManager,
+    FileManager
+}
