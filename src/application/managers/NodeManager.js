ee222fe6108dfbaac03537f0b58863e778c441d1
NaruDevnote <pokeraven4@gmail.com>
2021-08-19 23:54:52 +0100

    feat(managers): implement more methods

--- src/application/managers/NodeManager.js
+++ src/application/managers/NodeManager.js
@@ -1,22 +1,67 @@
 const { Node } = require('../../structures');
+const endpoints = require('./Endpoints');
 
 class NodeManager {
     constructor(client) {
         this.client = client;
 
         /**
-         * @type {Map<string, Node>}
+         * @type {Map<number, Node>}
          */
         this.cache = new Map();
     }
 
-    async fetch(id = '', force = false) {}
+    _patch(data) {
+        if (data.data && Array.isArray(data)) {
+            const s = new Map();
+            for (const o of data.data) {
+                const n = new Node(this.client, o);
+                this.cache.set(n.id, n);
+                s.set(n.id, n);
+            }
+            return s;
+        }
+        const n = new Node(this.client, data);
+        this.cache.set(n.id, n);
+        return n;
+    }
 
+    /**
+     * Fetches a node from the Pterodactyl API with an optional cache check.
+     * @param {number} [id] The ID of the node.
+     * @param {boolean} [force] Whether to skip checking the cache and fetch directly.
+     * @returns {Promise<Node|Map<number, Node>>}
+     */
+    async fetch(id, force = false) {
+        if (id) {
+            if (!force) {
+                const n = this.cache.get(id);
+                if (n) return Promise.resolve(n);
+            }
+            const data = await this.client.requests.make(endpoints.nodes.get(id));
+            return this._patch(data);
+        }
+        const data = await this.client.requests.make(endpoints.nodes.main);
+        return this._patch(data);
+    }
+
+    /** @todo */
     async create(options) {}
 
+    /** @todo */
     async update(id, options) {}
 
-    async delete(id) {}
+    /**
+     * Deletes a node from Pterodactyl.
+     * @param {number|Node} node The node to delete.
+     * @returns {number}
+     */
+    async delete(node) {
+        if (node instanceof Node) node = node.id;
+        await this.client.requests.make(endpoints.nodes.get(node), { method: 'DELETE' });
+        this.cache.delete(node);
+        return node;
+    }
 }
 
 module.exports = NodeManager;
--- src/application/managers/UserManager.js
+++ src/application/managers/UserManager.js
@@ -1,24 +1,95 @@
 const { PteroUser } = require('../../structures');
+const endpoints = require('./Endpoints');
 
 class UserManager {
     constructor(client) {
         this.client = client;
 
         /**
-         * @type {Map<string, PteroUser>}
+         * @type {Map<number, PteroUser>}
          */
         this.cache = new Map();
     }
 
-    async fetch(id = '', force = false) {}
+    /**
+     * Fetches a user from the Pterodactyl API with an optional cache check.
+     * @param {number} [id] The ID of the user.
+     * @param {boolean} [force] Whether to skip checking the cache and fetch directly.
+     * @returns {Promise<PteroUser|Map<number, PteroUser>>}
+     */
+    async fetch(id, force = false) {
+        if (id) {
+            if (!force) {
+                const u = this.cache.get(id);
+                if (u) return Promise.resolve(u);
+            }
+            let user = await this.client.requests.make(endpoints.users.get(id));
+            user = new PteroUser(this.client, user);
+            this.cache.set(user.id, user);
+            return user;
+        }
 
-    async fetchExternal(id, withServers = true) {}
+        const data = await this.client.requests.make(endpoints.users.main);
+        if (!Array.isArray(data)) throw new Error('Invalid API Response.');
 
-    async create(email, username, firstname, lastname) {}
+        const res = new Map();
+        data.forEach(o => {
+            const u = new PteroUser(this.client, o);
+            this.cache.set(u.id, u);
+            res.set(u.id, u);
+        });
+        return res;
+    }
 
+    /**
+     * Fetches a user by their external ID with an optional cache check.
+     * @param {number} id The ID of the external user.
+     * @param {object} [options] Additional fetch options.
+     * @param {boolean} [options.force] Whether to skip checking the cache and fetch directly.
+     * @param {boolean} [options.withServers] Whether to include servers the user has.
+     * @returns {Promise<PteroUser>}
+     */
+    async fetchExternal(id, options = {}) {
+        if (options.force !== true) this.cache.forEach(u => { if (id === u.externalId) return u });
+        let user = await this.client.requests.make(endpoints.users.ext(id) + (options.withServers ? '?include=servers' : ''));
+        user = new PteroUser(this.client, user);
+        this.cache.set(user.id, user);
+        return user;
+    }
+
+    /**
+     * Creates a new Pterodactyl user account.
+     * @param {string} email The email for the account.
+     * @param {string} username The username for the acount.
+     * @param {string} firstname The firstname for the account.
+     * @param {string} lastname The lastname for the account.
+     * @returns {Promise<PteroUser>}
+     */
+    async create(email, username, firstname, lastname) {
+        const data = await this.client.requests.make(
+            endpoints.users.main,
+            { email, username, first_name: firstname, last_name: lastname },
+            'POST'
+        );
+        const user = new PteroUser(this.client, data);
+        this.cache.set(user.id, user);
+        return user;
+    }
+
+    /** @todo */
     async update(options) {}
 
-    async delete(id) {}
+    /**
+     * Deletes the user account from Pterodactyl.
+     * @param {number|PteroUser} user The user to delete.
+     * @returns {number}
+     */
+    async delete(user) {
+        if (user instanceof PteroUser) user = user.id;
+        await this.client.requests.make(endpoints.users.get(user), { method: 'DELETE' });
+        this.cache.delete(user);
+        return user;
+    }
 }
 
 module.exports = UserManager;
