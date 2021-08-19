5e11570c756aad7a3c1cc135811bd75b833f0a9e
NaruDevnote <pokeraven4@gmail.com>
2021-08-19 23:53:21 +0100

    fix(managers): update index

--- src/application/managers/index.js
+++ src/application/managers/index.js
@@ -1,9 +1,11 @@
 const NodeLocationManager = require('./NodeLocationManager');
 const NodeManager = require('./NodeManager');
+const RequestManager = require('./RequestManager');
 const UserManager = require('./UserManager');
 
 module.exports = {
     NodeLocationManager,
     NodeManager,
+    RequestManager,
     UserManager
 }
