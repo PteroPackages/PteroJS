6da2a8896baa46c057d6d856e006d5470ca27afb
NaruDevnote <pokeraven4@gmail.com>
2021-08-19 17:18:05 +0100

    feat(structures): add files to index

--- src/structures/index.js
+++ src/structures/index.js
@@ -1,7 +1,10 @@
 const ApplicationServer = require('./ApplicationServer');
 const AllocationManager = require('./AllocationManager');
 const FileManager = require('./FileManager');
+const ClientServer = require('./ClientServer');
+const ClientUser = require('./ClientUser');
 const DatabaseManager = require('./DatabaseManager');
+const Schedule = require('./Schedule');
 const { BaseUser, PteroUser, PteroSubUser } = require('./User');
 const { Permissions, UserPermissions } = require('./Permissions');
 
@@ -9,7 +12,10 @@ module.exports = {
     ApplicationServer,
     AllocationManager,
     FileManager,
+    ClientServer,
+    ClientUser,
     DatabaseManager,
+    Schedule,
     BaseUser,
     PteroUser,
     PteroSubUser,
