2a2f84495fa302f67e7acce0a62ed2f8e810a2bd
NaruDevnote <pokeraven4@gmail.com>
2021-08-19 22:19:52 +0100

    feat(client): create endpoints

---
+++ src/application/managers/Endpoints.js
@@ -0,0 +1,36 @@
+module.exports = {
+    users:{
+        main: '/api/application/users',
+        get: u => `/api/application/user/${u}`,
+        ext: u => `/api/application/users/external/${u}`
+    },
+    nodes:{
+        main: '/api/application/nodes',
+        get: n => `/api/application/nodes/${n}`,
+        config: n => `/api/application/nodes/${n}/configuration`
+    },
+    servers:{
+        main: '/api/application/servers',
+        get: s => `/api/application/servers/${s}`,
+        ext: s => `/api/application/servers/external/${s}`,
+        details: s => `/api/application/servers/${s}/details`,
+        build: s => `/api/application/servers/${s}/build`,
+        startup: s => `/api/application/servers/${s}/startup`,
+        suspend: s => `/api/application/servers/${s}/suspend`,
+        unsuspend: s => `/api/application/servers/${s}/unsuspend`,
+        reinstall: s => `/api/application/servers/${s}/reinstall`,
+        force: s => `/api/application/servers/${s}/force`
+    },
+    locations:{
+        main: '/api/application/locations',
+        get: l => `/api/application/locations/${l}`
+    },
+    nests:{
+        main: '/api/application/nests',
+        get: n => `/api/application/nests/${n}`,
+        eggs:{
+            main: e => `/api/application/nests/${e}/eggs`,
+            get: (n, e) => `/api/application/nests/${n}/eggs/${e}`
+        }
+    }
+}
