98bb185479988d7779bbb7bc7f16cb772b11f758
NaruDevnote <pokeraven4@gmail.com>
2021-08-20 06:09:42 +0100

    feat(app): implement NestManager & fetch option

--- src/application/PteroApp.js
+++ src/application/PteroApp.js
@@ -1,4 +1,5 @@
 const {
+    NestManager,
     NodeLocationManager,
     NodeManager,
     RequestManager,
@@ -24,6 +25,7 @@ class PteroApp {
 
         this.users = new UserManager(this);
         this.nodes = new NodeManager(this);
+        this.nests = new NestManager(this);
         this.servers = new ServerManager(this);
         this.locations = new NodeLocationManager(this);
     }
@@ -31,6 +33,7 @@ class PteroApp {
     async connect() {
         if (this.options.fetchUsers) await this.users.fetch();
         if (this.options.fetchNodes) await this.nodes.fetch();
+        if (this.options.fetchNests) await this.nests.fetch();
         if (this.options.fetchServers) await this.servers.fetch();
         if (this.options.fetchLocations) await this.locations.fetch();
         this.readyAt = Date.now();
@@ -45,6 +48,7 @@ module.exports = PteroApp;
  * @typedef {object} ApplicationOptions
  * @property {boolean} [fetchUsers] Whether to fetch all users.
  * @property {boolean} [fetchNodes] Whether to fetch all nodes.
+ * @property {boolean} [fetchNests] Whether to fetch all nests.
  * @property {boolean} [fetchServers] Whether to fetch all servers.
  * @property {boolean} [fetchLocations] Whether to fetch all node locations.
  */
