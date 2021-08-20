f300333374e72529afac2239b7afe9e9dec4ef25
NaruDevnote <pokeraven4@gmail.com>
2021-08-20 03:00:36 +0100

    feat(locations): implement methods

--- src/application/managers/NodeLocationManager.js
+++ src/application/managers/NodeLocationManager.js
@@ -1,3 +1,5 @@
+const endpoints = require('./Endpoints');
+
 class NodeLocationManager {
     constructor(client) {
         this.client = client;
@@ -8,13 +10,94 @@ class NodeLocationManager {
         this.cache = new Map();
     }
 
-    async fetch(id, force = false) {}
+    _patch(data) {
+        if (data.data && Array.isArray(data)) {
+            const s = new Map();
+            for (const o of data.data) {
+                this.cache.set(o.id, {
+                    id: o.id,
+                    long: o.long,
+                    short: o.short,
+                    createdAt: new Date(o.created_at),
+                    updatedAt: o.updated_at ? new Date(o.updated_at) : null
+                });
+                s.set(o.id, this.cache.get(o.id));
+            }
+            return s;
+        }
+        data = data.attributes;
+        this.cache.set(data.id, {
+            id: data.id,
+            long: data.long,
+            short: data.short,
+            createdAt: new Date(data.created_at),
+            updatedAt: data.updated_at ? new Date(data.updated_at) : null
+        });
+        return this.cache.get(data.id);
+    }
 
-    async create(short, long) {}
+    /**
+     * Fetches a node location from the Pterodactyl API with an optional cache check.
+     * @param {number} [id] The ID of the location.
+     * @param {boolean} [force] Whether to skip checking the cache and fetch directly.
+     * @returns {Promise<NodeLocation|Map<number, NodeLocation>>}
+     */
+    async fetch(id, force = false) {
+        if (id) {
+            if (!force) {
+                const l = this.cache.get(id);
+                if (l) return l;
+            }
+            const data = await this.client.requests.make(endpoints.locations.get(id));
+            return this._patch(data);
+        }
+        const data = await this.client.requests.make(endpoints.locations.main);
+        return this._patch(data);
+    }
 
-    async update(id, { short, long } = {}) {}
+    /**
+     * Creates a new node location.
+     * @param {string} short The short location code of the location.
+     * @param {string} long The long location code of the location.
+     * @returns {Promise<NodeLocation>}
+     */
+    async create(short, long) {
+        return this._patch(
+            await this.client.requests.make(
+                endpoints.locations.main,
+                { short, long },
+                'POST'
+            )
+        );
+    }
 
-    async delete(id) {}
+    /**
+     * Updates an existing node location.
+     * @param {number} id The ID of the node location.
+     * @param {string} short The short location code of the location.
+     * @param {string} long The long location code of the location.
+     * @returns {Promise<NodeLocation>}
+     */
+    async update(id, { short, long } = {}) {
+        return this._patch(
+            await this.client.requests.make(
+                endpoints.locations.get(id),
+                { short, long },
+                'PATCH'
+            )
+        );
+    }
+
+    /**
+     * Deletes a node location.
+     * @param {number} id The ID of the node location.
+     * @returns {Promise<number>}
+     */
+    async delete(id) {
+        await this.client.requests.make(endpoints.locations.get(id), { method: 'DELETE' });
+        this.cache.delete(id);
+        return id;
+    }
 }
 
 module.exports = NodeLocationManager;
@@ -22,11 +105,11 @@ module.exports = NodeLocationManager;
 /**
  * Represents a location on Pterodactyl.
  * Location objects have little to no methodic usage so they are readonly.
+ * @readonly
  * @typedef {object} NodeLocation
  * @property {number} id The ID of the location.
  * @property {string} long The long location code.
  * @property {string} short The short location code (or country code).
  * @property {Date} createdAt The date the location was created.
  * @property {?Date} updatedAt The date the location was last updated.
- * @readonly
  */
