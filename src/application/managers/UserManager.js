f5aa2d9398463ce572f35045a10ae675e26c2e72
NaruDevnote <pokeraven4@gmail.com>
2021-08-21 19:26:03 +0100

    feat(usermanager): implement update() method

--- src/application/managers/UserManager.js
+++ src/application/managers/UserManager.js
@@ -76,8 +76,44 @@ class UserManager {
         return this._patch(data);
     }
 
-    /** @todo */
-    async update(options) {}
+    /**
+     * Updates the specified user's account.
+     * @param {number|PteroUser} user The user to update.
+     * @param {object} options Changes to update the user with.
+     * @param {string} [options.email] The new email for the account.
+     * @param {string} [options.username] The new username for the account.
+     * @param {string} [options.firstname] The new firstname for the account.
+     * @param {string} [options.lastname] The new lastname for the account.
+     * @param {string} [options.language] The new language for the account.
+     * @param {string} options.password The password for the user account.
+     * @returns {Promise<PteroUser>}
+     */
+    async update(user, options) {
+        if (!options.password) throw new Error('User password is required.');
+        if (
+            !options.email &&
+            !options.username &&
+            !options.firstname &&
+            !options.lastname &&
+            !options.language
+        ) throw new Error('Too few parameters to update.');
+        if (typeof user === 'number') user = await this.fetch(user);
+
+        const { password } = options;
+        let { email, username, firstname, lastname, language } = user;
+        if (options.email) email = options.email;
+        if (options.username) username = options.username;
+        if (options.firstname) firstname = options.firstname;
+        if (options.lastname) lastname = options.lastname;
+        if (options.language) language = options.language;
+
+        const data = await this.client.requests.make(
+            endpoints.users.get(user.id),
+            { email, username, first_name: firstname, last_name: lastname, language, password },
+            'PATCH'
+        );
+        return this._patch(data);
+    }
 
     /**
      * Deletes the user account from Pterodactyl.
