03e7a595caa3282cea34d9e28a108a856ffac106
NaruDevnote <pokeraven4@gmail.com>
2021-08-22 01:02:09 +0100

    feat(requestmanager): implement raw and support 201 & 204

--- src/client/managers/RequestManager.js
+++ src/client/managers/RequestManager.js
@@ -8,30 +8,38 @@ class RequestManager {
         this.headers['Authorization'] = `Bearer ${client.auth}`;
     }
 
-    reset = 0;
+    suspended = false;
     headers = {
         'User-Agent': `Client PteroJS v${version}`,
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
 
+    /**
+     * @param {string} url
+     * @param {object} [params]
+     * @param {string} [method]
+     * @returns {Promise<object|boolean>}
+     */
     async make(url, params = {}, method = 'GET') {
-        if (this.reset > Date.now()) throw new RequestError('Client is ratelimited.');
+        if (this.suspended) throw new RequestError('Client is ratelimited.');
+        const body = params.raw ?? JSON.stringify(params);
         const data = await fetch(this.client.domain + url, {
             method,
-            body: JSON.stringify(params),
+            body,
             headers: this.headers
         });
 
         if ([401, 403, 429].includes(data.status)) {
             if (data.status === 401) throw new RequestError();
-            if (data.status === 403) throw new RequestError('403: Path forbidden.');
-            
+            if (data.status === 403) throw new RequestError('Path is forbidden.');
+
             this.suspended = true;
-            this.timeout = setTimeout(() => this.suspended = false, 600000);
-            throw new RequestError('Ratelimited, resolving in 10 minutes.');
+            setTimeout(() => this.suspended = false, 600000);
+            throw new RequestError('Client is ratelimited, restarting in 10 minutes.');
         }
 
+        if ([201, 204].includes(data.status)) return true;
         if (data.ok) return await data.json();
         return null;
     }
