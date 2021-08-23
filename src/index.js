8fb2ff59b6fb2523ecd7dd6bdcbb3cc73aeea340
NaruDevnote <pokeraven4@gmail.com>
2021-08-23 06:39:23 +0100

    style(pkg): update package index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const PteroApp = require('./application/PteroApp');
-const PteroClient = require('./client/PteroClient');
-
-module.exports = { PteroApp, PteroClient };
+module.exports = {
+    PteroApp: require('./application/PteroApp'),
+    PteroClient: require('./client/PteroClient')
+};
