5ab35d9fe5badc86b0e345e81a14ccaba52c0a35
NaruDevnote <pokeraven4@gmail.com>
2021-08-25 17:11:21 +0100

    refactor(pkg): export working structures & classes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 module.exports = {
+    AllocationManager: require('./managers/AllocationManager'),
+    ApplicationRequestManager: require('./application/managers/ApplicationRequestManager'),
+    ApplicationServer: require('./structures/ApplicationServer'),
+    BackupManager: require('./client/managers/BackupManager'),
+    BaseUser: require('./structures/User').BaseUser,
+    ClientRequestManager: require('./client/managers/ClientRequestManager'),
+    ClientServer: require('./structures/ClientServer'),
+    ClientUser: require('./structures/User').ClientUser,
+    DatabaseManager: require('./managers/DatabaseManager'),
+    Errors: require('./structures/Errors'),
+    FileManager: require('./managers/FileManager'),
+    Node: require('./structures/Node'),
+    NodeLocationManager: require('./application/managers/NodeLocationManager'),
+    NodeManager: require('./application/managers/NestManager'),
+    Permissions: require('./structures/Permissions'),
     PteroApp: require('./application/PteroApp'),
-    PteroClient: require('./client/PteroClient')
+    PteroClient: require('./client/PteroClient'),
+    PteroSubUser: require('./structures/User').PteroSubUser,
+    PteroUser: require('./structures/User').PteroUser,
+    Schedule: require('./structures/Schedule')
 };
