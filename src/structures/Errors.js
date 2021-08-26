a9e2a121a5389264f2461411d04b28c9b1610a0f
NaruDevnote <pokeraven4@gmail.com>
2021-08-26 03:52:21 +0100

    feat(errors): fix & PteroAPIError

--- src/structures/Errors.js
+++ src/structures/Errors.js
@@ -1,3 +1,11 @@
 exports.RequestError = class RequestError extends Error {
-    constructor(message = 'Invalid request to API.') { super(message) };
+    constructor(message) { super(message) };
+}
+
+exports.PteroAPIError = class PteroAPIError extends Error {
+    constructor(data) {
+        data = data.errors[0];
+        super(`[${data.status}] ${data.detail}\nError Code: ${data.code}`);
+        this.code = data.code;
+    }
 }
