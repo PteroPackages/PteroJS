fbb7f82945f3a1fd3dde998cccdef98ae875b39a
NaruDevnote <pokeraven4@gmail.com>
2021-09-11 02:30:36 +0100

    refactor(AllocationManager): rewrite most of file

--- src/managers/AllocationManager.js
+++ src/managers/AllocationManager.js
@@ -1,34 +1,94 @@
+const endpoints = require('../client/managers/endpoints');
+
 class AllocationManager {
-    constructor(data) {
+    constructor(client, server, data) {
+        this.client = client;
+        this.server = server;
+
+        /**
+         * Whether the client using this manager is the PteroClient or PteroApp.
+         * @type {boolean}
+         */
+        this.isClient = client.constructor.name === 'PteroClient';
+
         /** @type {Set<Allocation>} */
         this.cache = new Set();
         this._patch(data);
     }
 
     _patch(data) {
-        if (!data) return;
-        for (let o of data.relationships.allocations.data) {
-            o = o.attributes;
-            this.cache.add({
-                id: o.id,
-                ip: o.ip,
-                ipAlias: o.ip_alias ?? null,
-                port: o.port,
-                notes: o.notes || null,
-                isDefault: o.is_default
-            });
+        if (!data.allocations && !data.data && !data.attributes) return;
+        if (data.allocations) data = data.allocations;
+        if (data.data) {
+            const res = new Set();
+            for (let o of data.data) {
+                o = o.attributes;
+                res.add({
+                    id: o.id,
+                    ip: o.ip,
+                    ipAlias: o.ip_alias,
+                    port: o.port,
+                    notes: o.notes ?? null,
+                    isDefault: o.is_default
+                });
+            }
+            res.forEach(a => this.cache.add(a));
+            return res;
+        } else {
+            data = data.attributes;
+            const o = {
+                id: data.id,
+                ip: data.ip,
+                ipAlias: data.ip_alias,
+                port: data.port,
+                notes: data.notes ?? null,
+                isDefault: data.is_default
+            }
+            this.cache.add(o);
+            return o;
         }
     }
 
-    async fetch() {}
+    async fetch() {
+        return this._patch(
+            await this.client.requests.make(
+                endpoints.servers.network.main(this.server.identifier)
+            )
+        );
+    }
 
-    async assign() {}
+    async assign() {
+        return this._patch(
+            await this.client.requests.make(
+                endpoints.servers.network.main(this.server.identifier), { method: 'POST' }
+            )
+        );
+    }
 
-    async setNote(id, note) {}
+    async setNote(id, note) {
+        const data = await this.client.requests.make(
+            endpoints.servers.network.get(this.server.identifier, id),
+            { notes: note }, 'POST'
+        );
+        return this._patch(data);
+    }
 
-    async setPrimary(id) {}
+    async setPrimary(id) {
+        return this._patch(
+            await this.client.requests.make(
+                endpoints.servers.network.primary(this.server.identifier, id), { method: 'POST' }
+            )
+        );
+    }
 
-    async unassign(id) {}
+    async unassign(id) {
+        await this.client.requests.make(
+            endpoints.servers.network.get(this.server.identifier, id), { method: 'DELETE' }
+        );
+        const res = this.cache;
+        this.cache.forEach(a => { if (a.id !== id) res.add(a) });
+        this.cache = a;
+    }
 }
 
 module.exports = AllocationManager;
