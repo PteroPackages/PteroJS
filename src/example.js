7fdea79519fb31952d78025685c0ca7546de94ad
devnote-dev <pokeraven4@gmail.com>
2021-10-20 19:51:07 +0100

    chore: update example with latest version

--- src/example.js
+++ src/example.js
@@ -2,27 +2,24 @@
 const { NodeStatus } = require('.');
 
 const client = new NodeStatus({
-    name: 'NODE_NAME',
     domain: 'PTERO_DOMAIN',
     auth: 'PTERO_APPLICATION_API_KEY',
-    interval: 60, // The interval in seconds between status calls
+    nodes: [2, 3], // The IDs of the nodes to listen for
+    callInterval: 60, // The interval in seconds between API calls
     retryLimit: 2 // The number of times to retry on unsuccessful calls
 });
 
-// Emitted when a connection is made
-client.on('connect', () => console.log(`Connected to node ${client.name}`));
+// Emitted when a connection is made to a node
+client.on('connect', id => console.log(`Connected to node ${id}!`));
 
 // Emitted when the connection is lost or unavailable
-client.on('disconnect', msg => console.log(`Node disconncted: ${msg}`));
-
-// Emitted when there is an error during the process
-client.on('error', console.error);
+client.on('disconnect', id => console.log(`Disconnected from node ${id}.`));
 
 // Emitted with the node data per interval
 client.on('interval', data => {
     const { id, name, memory, disk } = data;
-    console.log(
-        `Node Information:
+    console.log(`
+        Node Information:
         ID: ${id}
         Name: ${name}
         Memory: ${memory}MB
@@ -34,7 +31,6 @@ client.on('interval', data => {
 // which will operate the same way as the client.on() function:
 // - client.onConnect
 // - client.onDisconnect
-// - client.onError
 // - client.onInterval
 //
 // The events will be emitted first before the function is called
