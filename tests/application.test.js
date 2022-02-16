93448414d297ef993b19a6a0851424feeecda666
devnote-dev <inferno0745@gmail.com>
2022-02-16 22:41:40 +0000

    refactor(*): use node assert for tests

--- tests/application.test.js
+++ tests/application.test.js
@@ -1,26 +1,22 @@
 const assert = require('assert');
-const { PteroClient } = require('../src');
+const { PteroApp } = require('../src');
 const { api_url, api_key } = require('./auth.json');
 
-const app = new PteroClient(api_url, api_key);
+const app = new PteroApp(api_url, api_key);
 
 assert.doesNotThrow(
     (async () => await app.connect()),
     'could not connect to api'
 );
-
 assert.ok(app.ping, 'ping not calculated');
-
-assert.doesNotThrow(
-    (async () => await app.fetchClient()),
-    'could not fetch users endpoint'
-);
-
-assert.ok(app.user, 'user not fetched');
 
 assert.doesNotThrow(
     (async () => await app.servers.fetch()),
     'could not fetch servers endpoint'
+);
+assert.doesNotThrow(
+    (async () => await app.users.fetch()),
+    'could not fetch users endpoint'
 );
 
 app.disconnect();
--- tests/index.js
+++ tests/index.js
@@ -1,49 +1,13 @@
-class AssertionError extends Error {
-    constructor() { super('Assertion failed') }
-}
-
-function logTest(test) {
-    console.log(
-        '-'.repeat(15 + test.baseReturn.length) +'\n'+
-        `Running Test\nName        : ${test.name}\n`+
-        `Description : ${test.description}\n`+
-        `Return      : ${test.baseReturn}\n`+
-        '-'.repeat(15 + test.baseReturn.length)
-    );
-}
-
-function assert(ops) {
-    if (Boolean(ops) !== true) throw new AssertionError();
-}
-
-function test(name, exec, returnValue = null) {
-    this.meta = {
-        name,
-        description: null,
-        group: null,
-        baseReturn: (returnValue && typeof returnValue) || 'null'
-    }
-
-    logTest(this.meta);
-    if (returnValue !== undefined) {
-        assert(exec() === returnValue);
-    } else {
-        assert(exec());
-    }
-}
-
-module.exports = { test, assert };
-
 (async () => {
     console.log('Running all tests...');
     const { readdirSync } = require('fs');
-    let count = 0, test;
+    let count = 0;
+
     for (const mod of readdirSync(__dirname)) {
         if (!mod.endsWith('.test.js')) continue;
+
         console.log(`\nRunning Test #${count++}\n`+ '='.repeat(20));
-        test = require(`${__dirname}/${mod}`);
-        console.log(test, '\n');
-        if (test) await test(); else 'Test Skipped...';
+        require(`${__dirname}/${mod}`);
     }
     console.log('Completed all tests.');
 })();
