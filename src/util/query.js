aa45c785b07aef4d1026183fcb882a33de57af5c
devnote-dev <inferno0745@gmail.com>
2022-03-17 20:58:45 +0000

    fix(util): formatting & loop fix

--- src/util/query.js
+++ src/util/query.js
@@ -10,23 +10,26 @@ module.exports = (args, allowed) => {
     if (args.page) {
         if (args.page < 1 || args.page > 50)
             throw new RangeError('Page query must be between 1 and 50.');
+
         parsed.push(`page=${args.page}`);
     }
 
     if (args.perPage) {
         if (args.perPage < 1 || args.perPage > 100)
             throw new RangeError('PerPage query must be between 1 and 100.');
+
         parsed.push(`per_page=${args.perPage}`);
     }
 
     if (args.filter) {
         if (!allowed.filters.includes(args.filter[0]))
             throw new SyntaxError(`Invalid filter argument '${args.filter[0]}'.`);
+
         parsed.push(`filter[${args.filter[0]}]=${args.filter[1]}`);
     }
 
     if (args.include) {
-        for (const arg in args.include) {
+        for (const arg of args.include) {
             if (!allowed.include.includes(arg))
                 throw new SyntaxError(`Invalid include argument '${arg}'.`);
         }
@@ -36,6 +39,7 @@ module.exports = (args, allowed) => {
     if (args.sort) {
         if (!allowed.sorts.includes(args.sort))
             throw new SyntaxError(`Invalid sort argument '${args.sort}'.`);
+
         parsed.push(`sort=${args.sort}`);
     }
 
