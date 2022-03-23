687450ab17c4b05043850025ab16c3780a5a8bbe
devnote-dev <inferno0745@gmail.com>
2022-03-23 23:36:30 +0000

    refactor: update package index

--- src/index.js
+++ src/index.js
@@ -2,44 +2,46 @@ module.exports = {
     version: require('../package.json').version,
 
     // Application API
+    ApplicationServerManager: require('./application/ApplicationServerManager'),
+    NestEggsManager: require('./application/NestEggsManager'),
+    NestManager: require('./application/NestManager'),
+    NodeAllocationManager: require('./application/NodeAllocationManager'),
+    NodeLocationManager: require('./application/NodeLocationManager'),
+    NodeManager: require('./application/NodeManager'),
     PteroApp: require('./application/PteroApp'),
     UserManager: require('./application/UserManager'),
-    NodeManager: require('./application/NodeManager'),
-    NestManager: require('./application/NestManager'),
-    NestEggsManager: require('./application/NestEggsManager'),
-    ApplicationServerManager: require('./application/ApplicationServerManager'),
-    NodeLocationManager: require('./application/NodeLocationManager'),
 
     // Client API
-    PteroClient: require('./client/PteroClient'),
-    ClientServerManager: require('./client/ClientServerManager'),
-    ScheduleManager: require('./client/ScheduleManager'),
-    BackupManager: require('./client/BackupManager'),
-    SubUserManager: require('./client/SubUserManager'),
-
-    // Websocket
     Shard: require('./client/ws/Shard'),
     WebSocketManager: require('./client/ws/WebSocketManager'),
 
-    // Global Managers
-    DatabaseManager: require('./client/ClientDatabaseManager'),
+    BackupManager: require('./client/BackupManager'),
+    ClientDatabaseManager: require('./client/ClientDatabaseManager'),
+    ClientServerManager: require('./client/ClientServerManager'),
     FileManager: require('./client/FileManager'),
+    NetworkAllocationManager: require('./client/NetworkAllocationManager'),
+    PteroClient: require('./client/PteroClient'),
+    ScheduleManager: require('./client/ScheduleManager'),
+    SubUserManager: require('./client/SubUserManager'),
+
+    // Extensions
+    NodeStatus: require('./extensions/NodeStatus'),
+
+    // HTTP
     RequestManager: require('./http/RequestManager'),
 
-    // Package Structures
+    // Structures
     ApplicationServer: require('./structures/ApplicationServer'),
     ClientServer: require('./structures/ClientServer'),
     Dict: require('./structures/Dict'),
+    ...require('./structures/Errors'),
     Node: require('./structures/Node'),
     Permissions: require('./structures/Permissions'),
     Schedule: require('./structures/Schedule'),
-    ...require('./structures/Errors'),
     ...require('./structures/User'),
 
-    // Utils
-    configLoader: require('./util/configLoader'),
+    // Utility
     caseConv: require('./util/caseConv'),
-
-    // Extensions
-    NodeStatus: require('./extensions/NodeStatus')
+    configLoader: require('./util/configLoader'),
+    query: require('./util/query')
 };
