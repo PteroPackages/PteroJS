5c49b3bb54d7e7dedf642fecda2e4efd9ed08368
devnote-dev <inferno0745@gmail.com>
2022-04-06 05:20:04 +0100

    feat(structures): implement base manager for all managers

---
+++ src/structures/BaseManager.ts
@@ -0,0 +1,13 @@
+export default abstract class BaseManager {
+    abstract get FILTERS(): Readonly<string[]>;
+    abstract get SORTS(): Readonly<string[]>;
+    abstract get INCLUDES(): Readonly<string[]>;
+
+    getQueryOptions() {
+        return {
+            filters: this.FILTERS ?? [],
+            sorts: this.SORTS ?? '',
+            includes: this.INCLUDES ?? []
+        }
+    }
+}
