919d865740512b5715495fe5aac5e81170c2cd1a
devnote-dev <inferno0745@gmail.com>
2022-05-14 11:14:40 +0100

    fix(ws): typescript moment

--- src/client/ws/packetHandler.ts
+++ src/client/ws/packetHandler.ts
@@ -6,11 +6,11 @@ export default function (shard: Shard, payload: WebSocketPayload): void {
     const args = payload.args || [];
 
     switch (payload.event) {
-        case 'status': shard.emit('statusUpdate', ...args); break;
-        case 'console output': shard.emit('serverOutput', ...args); break;
-        case 'daemon message': shard.emit('daemonMessage', ...args); break;
+        case 'status': shard.emit('statusUpdate', args.join()); break;
+        case 'console output': shard.emit('serverOutput', args.join()); break;
+        case 'daemon message': shard.emit('daemonMessage', args.join()); break;
         case 'install started': shard.emit('installStart'); break;
-        case 'install output': shard.emit('installOutput', ...args); break;
+        case 'install output': shard.emit('installOutput', args.join()); break;
         case 'install completed': shard.emit('installComplete'); break;
         case 'stats':
             const stats = JSON.parse(args.join());
@@ -18,14 +18,14 @@ export default function (shard: Shard, payload: WebSocketPayload): void {
             break;
         case 'transfer logs':
         case 'transfer status':
-            shard.emit('transferUpdate', ...args); break;
+            shard.emit('transferUpdate', args.join()); break;
         case 'backup completed':
             const backup = JSON.parse(args.join());
             shard.emit('backupComplete', caseConv.toCamelCase(backup));
             break;
         case 'daemon error':
         case 'jwt error':
-            shard.emit('error', ...args); break;
+            shard.emit('error', args.join()); break;
         default:
             shard.emit('error', `received unknown event '${payload.event}'`);
             break;
